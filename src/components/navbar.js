103f234e34f5145931fd86afbb3cb989f3c558ab
silviaalam134 <150582995+silviaalam134@users.noreply.github.com>
2025-08-18 06:23:09 +0600

    login & registration page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,37 +1,47 @@
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import styles from "../css/myNav.module.css";
-const MyNav = () => {
-    return ( 
-        <Navbar expand="lg" className={`${styles.myColor} bg-body-tertiary`}>
-            <Container>
-                <Navbar.Brand href="#home">Warm Connect</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">About</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">
-                        Another action
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">
-                        Separated link
-                    </NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-                <Button className={styles.loginBtn} variant="outline-primary" href="#login">Login</Button> 
-                </Navbar.Collapse>
-            </Container>
+import { Link } from 'react-router-dom';
+import styles from '../css/myNav.module.css';
+
+const NavBarComponent = () => {
+  return (
+    <Navbar expand="lg" className={`${styles.myColor} bg-body-tertiary`}>
+      <Container>
+        {/* Brand */}
+        <Navbar.Brand as={Link} to="/">Warm Connect</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          {/* Links */}
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+
+          {/* Login button using React Router Link */}
+          <Button
+            as={Link}
+            to="/login"
+            className={styles.loginBtn}
+            variant="outline-primary"
+          >
+            Login
+          </Button>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
-     );
-}
- 
-export default MyNav;
+  );
+};
+
+export default NavBarComponent;
